394fbf7c4ce956bfd02744ef77d0bbd6116d34e3
Olivier Le Clerc <olivier.le-clerc@orange.fr>
2024-08-31 15:48:17 +0200

    single-item fixes

--- public/themes/wordplate/single-portfolio.php
+++ public/themes/wordplate/single-portfolio.php
@@ -7,7 +7,13 @@
             <div class="single-item">
                 <?php $images = get_portfolio_images() ?>
 
-                <h1 class="mobile-only"><?php the_title(); ?></h1>
+
+                <div class="item-description grid-description">
+                    <h1><?php the_title(); ?></h1>
+                    <p>
+                        <?= remove_images(get_the_content()) ?>
+                    </p>
+                </div>
 
                 <div class="grid-image">
                     <?php foreach (get_portfolio_images() as $image) : ?>
@@ -17,12 +23,7 @@
                     <?php endforeach ?>
                 </div>
 
-                <div class="item-description grid-description">
-                    <h1 class="desktop-only"><?php the_title(); ?></h1>
-                    <p>
-                        <?= remove_images(get_the_content()) ?>
-                    </p>
-                </div>
+
 
 
             </div>
